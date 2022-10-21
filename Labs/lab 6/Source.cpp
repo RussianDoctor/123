fff0780abb66f075421b1936dcbb742b4c7ccaf8
RussianDoctor <111242958+RussianDoctor@users.noreply.github.com>
2022-10-21 16:56:59 +0500

    Add files via upload

---
+++ Labs/lab 6/Source.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool stroka(string const& s)
+{
+    for (int i = 0; i < s.size(); i++)
+        if (!isdigit(s[i]))
+            return false;
+    return true;
+}
+int main()
+{
+    string s, word;
+    cout << "¬ведите строку\n";
+    getline(cin, s);
+    stringstream str(s);
+    while (str >> word)
+    {
+        if (stroka(word))
+            cout << "\b" << " ";
+        else
+            cout << word << " ";
+    }
+    system("pause");
+    return 0;
+}
